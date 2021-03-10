026cd7d20387ba7d68a1ccf06e5b65d66bfe1450
Maurya Gamage <mgam0010@student.monash.edu>
2021-03-10 10:11:39 +0530

    start

---
+++ Bootcamps/Bootcamp02/src/AutoShowroom.java
@@ -0,0 +1,6 @@
+public class AutoShowroom {
+    public void printStatus(){
+        System.out.println("Welcome to FIT2099 Showroom");
+        System.out.println("Thank you for visiting FIT2099 Showroom");
+    }
+}
