3818035a35f97c74990bb6d296bda9a9121f5b20
Maurya Gamage <mgam0010@student.monash.edu>
2021-03-18 20:36:32 +0530

    Added consoling i/o

--- Bootcamps/Bootcamp03/src/AutoShowroom.java
+++ Bootcamps/Bootcamp03/src/AutoShowroom.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AutoShowroom {
 
     ArrayList<Car> carArray = new ArrayList();
     ArrayList<Buyer> buyerArray = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
     public void printStatus() {
         System.out.println("Welcome to FIT2099 Showroom");
         createCars();
@@ -24,15 +26,36 @@ public class AutoShowroom {
         carArray.get(0).addBid(buyerArray.get(1), 25000, "12/12/2021" );
         carArray.get(1).addBid(buyerArray.get(2), 2000, "6/5/2021" );
 
+        System.out.println("Enter Buyer First name");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter Buyer Last name");
+        String givenName = scanner.nextLine();
+
+        System.out.println("Enter Buyer Id");
+        String buyerId = scanner.nextLine();
+
+        System.out.println("Enter Date");
+        String date = scanner.nextLine();
+
+        System.out.println("Enter Bid price");
+        int bidPrice = scanner.nextInt();
+
+        buyerArray.add(new Buyer(buyerId, firstName, givenName));
+        carArray.get(2).addBid(buyerArray.get(3), bidPrice, date );
+
     }
 
     public void displayCars(){
         for (int i =0; i < carArray.size(); i++) {
+
             System.out.println("Car (" +(i+1)+ ") " +carArray.get(i).description());
             if (carArray.get(i).bidCount > 0) {
+
                 int bids = carArray.get(i).bidCount;
                 System.out.println("Car (" + (i + 1) + ") has " + bids + " bids");
                 for (int j =0; j < bids; j++) {
+
                     Bid bidArray = (Bid) carArray.get(i).bids.get(j);
                     String buyer = bidArray.getBuyer().description();
                     System.out.println("Bid (" + (j + 1) + ") : " + bidArray.getBidId() + " " + buyer + " " + bidArray.getBidPrice() + " " + bidArray.getDate()) ;
