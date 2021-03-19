3869d597173366dc1c9659e9fbcbbbdc8508f4bd
Maurya Gamage <mgam0010@student.monash.edu>
2021-03-19 13:12:42 +0530

    Improved console to better reflect task

--- Bootcamps/Bootcamp03/src/AutoShowroom.java
+++ Bootcamps/Bootcamp03/src/AutoShowroom.java
@@ -25,24 +25,7 @@ public class AutoShowroom {
         carArray.get(0).addBid(buyerArray.get(0), 5000, "12/12/2020" );
         carArray.get(0).addBid(buyerArray.get(1), 25000, "12/12/2021" );
         carArray.get(1).addBid(buyerArray.get(2), 2000, "6/5/2021" );
-
-        System.out.println("Enter Buyer First name");
-        String firstName = scanner.nextLine();
-
-        System.out.println("Enter Buyer Last name");
-        String givenName = scanner.nextLine();
-
-        System.out.println("Enter Buyer Id");
-        String buyerId = scanner.nextLine();
-
-        System.out.println("Enter Date");
-        String date = scanner.nextLine();
-
-        System.out.println("Enter Bid price");
-        int bidPrice = scanner.nextInt();
-
-        buyerArray.add(new Buyer(buyerId, firstName, givenName));
-        carArray.get(2).addBid(buyerArray.get(3), bidPrice, date );
+        bidding();
 
     }
 
@@ -65,4 +48,34 @@ public class AutoShowroom {
             }
         }
     }
+
+    public void bidding(){
+        for (int i =0; i < carArray.size(); i++) {
+
+            System.out.println("Car (" + (i + 1) + ") " + carArray.get(i).description());
+
+            System.out.println("Enter 1 to bid or 0 to view next car");
+            int choice = Integer.parseInt(scanner.nextLine());
+            if (choice==1) {
+                System.out.println("Enter Buyer First name");
+                String firstName = scanner.nextLine();
+
+                System.out.println("Enter Buyer Last name");
+                String givenName = scanner.nextLine();
+
+                System.out.println("Enter Buyer Id");
+                String buyerId = scanner.nextLine();
+
+                System.out.println("Enter Date");
+                String date = scanner.nextLine();
+
+                System.out.println("Enter Bid price");
+                int bidPrice = Integer.parseInt(scanner.nextLine());
+
+                buyerArray.add(new Buyer(buyerId, firstName, givenName));
+                int buyer = buyerArray.size();
+                carArray.get(i).addBid(buyerArray.get(buyer - 1), bidPrice, date);
+            }
+            }
+        }
 }
